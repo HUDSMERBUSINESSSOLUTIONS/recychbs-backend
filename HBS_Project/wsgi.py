0df09630926dbeb68b53d480f29822bd5765f4bc
HBS <deployment@hudsmersolutions.com>
2024-12-09 11:56:40 +0530

    Update wsgi.py

--- HBS_Project/wsgi.py
+++ HBS_Project/wsgi.py
@@ -22,7 +22,7 @@ application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'stat
 
 
 # Add the React build directory to WhiteNoise
-application.add_files(os.path.join(settings.BASE_DIR, 'dist'), prefix='')  # Serve React's index.html and other root files
+#application.add_files(os.path.join(settings.BASE_DIR, 'dist'), prefix='')  # Serve React's index.html and other root files
 
 # Optionally, add React's static assets
-application.add_files(os.path.join(settings.BASE_DIR, 'dist/assets'), prefix='assets/')
+#application.add_files(os.path.join(settings.BASE_DIR, 'dist/assets'), prefix='assets/')
